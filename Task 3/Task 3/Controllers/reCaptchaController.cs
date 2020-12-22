d7db3a54235887000f88e25bec20f91ba4e00753
Astahn <aloysius_chng@hotmail.com>
2020-12-22 19:07:56 +0800

    Task 3 Milestone 1
    Done Authentication and Authorization with Bearer. Midway through adding reCaptcha v3 into the process.

---
+++ Task 3/Task 3/Controllers/reCaptchaController.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Task_3.Models;
+
+namespace Task_3.Controllers
+{
+    public class reCaptchaController : ApiController
+    {
+        [HttpPost]
+        [Route("api/recaptcha")]
+        public HttpResponseMessage checkRecaptcha(reCaptcha model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+        }
+
+        public static bool ReCaptchaPassed(string gRecaptcharesponse)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            var res = httpClient.GetAsync($"https://www.google.com/recaptcha/api/siteverify?secret=6Ld3HBAaAAAAAIc-T0fQ6Mkvb0njP3oNWPPC_68w&response={gRecaptcharesponse}").Result;
+
+            if (res.StatusCode != HttpStatusCode.OK)
+                return false;
+
+            string JSONres = res.Content.ReadAsStringAsync().Result;
+            dynamic JSONdata = JObject.Parse(JSONres);
+
+            if (JSONdata.success != "true")
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ Task 3/Task 3/Models/reCaptcha.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+
+namespace Task_3.Models
+{
+    public class reCaptcha
+    {
+        public string captcha { get; set; }
+    }
+}
